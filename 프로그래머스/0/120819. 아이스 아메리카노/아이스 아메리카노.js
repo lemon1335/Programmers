fe2206e47a85671029e0486d5e20a6899b687c7c
lemon1335 <kimjinhui114@gmail.com>
2024-11-13 11:21:31 +0900

    Update 아이스 아메리카노.js

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -4,3 +4,9 @@ function solution(money) {
     answer[1] = money - (5500 * answer[0]);
     return answer;
 }
+
+// 배열에 어떻게 넣을 지 고민했던 문제
+// 더 간단하고, 가독성 좋은 코드가 있어서 덧붙인다.
+function solution(money) {
+    return [Math.floor(money / 5500), money % 5500];
+}
