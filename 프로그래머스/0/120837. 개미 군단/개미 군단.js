858bf760edcf3fbff2672a8974d31f4071267c11
lemon1335 <kimjinhui114@gmail.com>
2024-11-14 10:30:15 +0900

    Update 개미 군단.js

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -4,3 +4,9 @@ function solution(hp) {
     let common = (hp - (5 * general) - (3 * solider));
     return general + solider + common;
 }
+
+// 순차적으로 나머지와 몫을 생각하면서 풀이해야 했던 문제
+// 더 간단한 풀이
+function solution(hp) {
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + (hp % 5) % 3;
+}
