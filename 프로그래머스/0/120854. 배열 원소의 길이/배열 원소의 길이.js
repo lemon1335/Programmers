c5d0a9c8cee7792885ea68786fa3783d50362bea
lemon1335 <kimjinhui114@gmail.com>
2024-11-15 15:28:47 +0900

    Update 배열 원소의 길이.js

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -5,3 +5,10 @@ function solution(strlist) {
    }
     return answer;
 }
+
+// 각 배열 원소의 길이를 구해야 했던 문제
+// 가독성이 더 좋은 코드
+function solution(strlist) {
+    return strlist.map((el) => el.length)
+}
+// 메소드를 사용하는 방향으로 고민해봐야겠다.
