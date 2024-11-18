ee4d52fabc7ad7896b9cfdd720cc406b0cd7b2ff
lemon1335 <kimjinhui114@gmail.com>
2024-11-18 17:00:51 +0900

    Update 가위 바위 보.js

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -10,3 +10,15 @@ function solution(rsp) {
         }
     } return result;
 }
+// map을 써야한다는 것 까지는 떠올렸지만, 어떻게 응용해야할 지 몰라서 if문으로 해결했던 문제.
+// 메소드도 언젠가는 쓸 날이 오겠지,,,,,,,,,,?
+// 객체와 map 메소드를 이용한 간단한 풀이
+function solution(rsp) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+}
