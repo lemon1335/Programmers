ee473078545631ad003af96b8ba546739f1d72a6
lemon1335 <kimjinhui114@gmail.com>
2024-11-19 16:28:02 +0900

    Update 가장 큰 수 찾기.js

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,3 +1,6 @@
 function solution(array) {
     return [Math.max(...array), array.indexOf(Math.max(...array))]
 }
+
+// 일단 배열을 펼치고, 가장 큰 수와 위치 찾기
+// 고민하는 시간을 가진 다음, 많이 찾아보고 알아보고 사용하는 것도 공부에 도움이 많이 되는 것 같다. 
