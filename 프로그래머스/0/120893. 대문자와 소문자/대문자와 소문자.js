9502a669bc7f994a4550e549098b5dbd5c20769b
lemon1335 <kimjinhui114@gmail.com>
2024-11-20 14:05:25 +0900

    Update 대문자와 소문자.js

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -8,3 +8,12 @@ function solution(my_string) {
         }
     } return answer.join('')
 }
+
+// toUpperCase(), toLowerCase() 메소드와 join() 메소드 활용
+// join() 메소드는 배열의 모든 요소를 연결해 하나의 문자열로 만든다.
+// if 문에서 탈출하고 싶다.......!
+
+// split() 메소드와 map() 메소드, 삼항연산자를 활용한 풀이
+function solution(my_string) {
+    return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('')
+}
