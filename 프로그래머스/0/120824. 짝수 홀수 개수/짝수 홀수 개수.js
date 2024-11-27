d8cc33a41fcf80eeeb912d5fdf46844a7f08bb0b
lemon1335 <kimjinhui114@gmail.com>
2024-11-27 12:16:45 +0900

    Update 짝수 홀수 개수.js

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,24 @@
 function solution(num_list) {
+   // even과 odd를 먼저 배열로 선언
    let even = [];
    let odd = [];
     for (let i = 0; i <num_list.length; i++) {
+       //배열을 2로 나눈 나머지값이 0 이면 짝수이기 때문에 even에 push
         if (num_list[i] % 2 === 0) {
             even.push(i)
         } else {
+           //그 외는 홀수이니까 odd에 push
             odd.push(i)
         }
-    } return [even.length, odd.length]
+    } 
+   // 그 후 even 배열의 길이 (개수) ,  odd 배열의 길이 (개수)를 return
+   return [even.length, odd.length]
+}
+
+// filter 메소드를 활용한 더 간단한 풀이
+function solution(num_list) {
+  return [
+    num_list.filter((num) => num % 2 === 0).length,
+    num_list.filter((num) => num % 2 === 1).length,
+  ];
 }
