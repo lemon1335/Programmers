094d27b8ca25aedd58355687b47bbebda476ee82
lemon1335 <kimjinhui114@gmail.com>
2024-11-28 12:00:13 +0900

    Update 숨어있는 숫자의 덧셈 （1）.js

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,8 +1,13 @@
 function solution(my_string) {
+ // 먼저 배열로 쪼개기 위한 변수 num을 선언한다.
  const num = my_string.split('');
+ // 쪼갠 배열의 숫자를 합산 할 변수 total을 선언한다.
  let total = 0;
     
     for (let i = 0; i < num.length; i++) {
+     // num[i]를 Number 함수를 사용하여 숫자로 변환하고, total에 그 값을 더한다.
         if (Number(num[i])) total += Number(num[i])
-    } return total
+    } 
+ // 최종적으로 total 반환
+ return total
 }
